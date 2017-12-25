99c8455931057dc9cc7f00099c65b662c37ac33c
weiwei <weiwei02@vip.qq.com>
2017-12-26 01:15:03 +0800

    创建简单Strem示例工程

--- spring-cloud-stream-learning/spring-cloud-stream-vote/src/main/java/com/github/weiwei02/springcloud/stream/vote/VoteRecordingSinkApplication.java
+++ spring-cloud-stream-learning/spring-cloud-stream-vote/src/main/java/com/github/weiwei02/springcloud/stream/vote/VoteRecordingSinkApplication.java
@@ -1,8 +1,13 @@
 package com.github.weiwei02.springcloud.stream.vote;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
 
 @SpringBootApplication
+@EnableBinding(Sink.class)
 public class VoteRecordingSinkApplication {
 
 	public static void main(String[] args) {
---
+++ spring-cloud-stream-learning/stream-simple-listener/src/test/java/com/github/weiwei02/springcloud/stream/simple/listener/SpringCloudStreamSimpleListenerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.weiwei02.springcloud.stream.simple.listener;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudStreamSimpleListenerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
