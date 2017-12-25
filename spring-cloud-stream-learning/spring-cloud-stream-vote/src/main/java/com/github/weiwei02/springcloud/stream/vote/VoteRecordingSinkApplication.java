e2484a5369e2f03854346cbe284a76f30b64a582
weiwei <weiwei02@vip.qq.com>
2017-12-26 00:25:50 +0800

    修改stream pom relation

---
+++ spring-cloud-stream-learning/spring-cloud-stream-vote/src/main/java/com/github/weiwei02/springcloud/stream/vote/VoteRecordingSinkApplication.java
@@ -0,0 +1,11 @@
+package com.github.weiwei02.springcloud.stream.vote;
+
+
+
+@SpringBootApplication
+public class VoteRecordingSinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteRecordingSinkApplication.class, args);
+	}
+}
